ee3ea59e09712815d47b13da44b3b924ebb7fc03
Test User <test.user@gmail.com>
2023-01-24 12:52:58 +0100

    Klasse main update 2 in branch module-1

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Bye world");
+        System.out.println("1 arbeiten in branch module-1");
+        System.out.println("Hallo from module-1");
     }
 }
